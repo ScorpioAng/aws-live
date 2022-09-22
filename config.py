2c47465f96e88b852cd4b923acef38cb6cfce942
ScorpioAng <113013045+ScorpioAng@users.noreply.github.com>
2022-09-22 12:51:18 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee-db.crmzpcr8khk9.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "abc12345"
+customdb = "employee"
+custombucket = "melvinangwaihin-bucket"
+customregion = "us-east-1"
